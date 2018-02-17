8c36aceca2458304b6f461b818175b8a51ac74fc
Sam Standing <samzstanding@gmail.com>
2018-02-16 18:46:43 -0600

    changed date display

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -39,7 +39,7 @@ function displayItems(obj) {
     output.empty();
     for (item of obj) {
         output.append(`<tr><td>${item.task}</td>
-        <td>${item.duedate.substring(0,10)}</td>
+        <td>${item.duedate.substring(5,10)}</td>
         <td>${item.notes}</td>
         <td>${item.complete}</td><td><button id="completeBtn" data-id="${item.id}">
         Mark Completed</button></td>
